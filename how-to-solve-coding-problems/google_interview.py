c90814237e705b8d12cf0791d79467c0c8f2f3cb
Vitor Messias <vitorhsm14@gmail.com>
2025-01-17 18:55:36 -0300

    Add better solution for google interview

--- how-to-solve-coding-problems/google_interview.py
+++ how-to-solve-coding-problems/google_interview.py
@@ -1,14 +1,29 @@
 from numbers import Number
 
-def naive_solution(array, sum: Number):
+def naive_solution(array, expected_sum: int):
     for i in array:
         for x in array:
-            if x + i == sum: return True
+            if x + i == expected_sum:
+                print(f'found [{i},{x}]')
+                return True
 
     return False
 
+def better_solution(array, expected_sum: int):
+    numbers = set()
+
+    for i in array:
+        if i in numbers:
+            print(f'found [{expected_sum - i},{i}]')
+            return True
+
+        numbers.add(expected_sum - i)
+
 def main():
     result = naive_solution([1, 2, 3, 4, 5, 6], 7)
+    print(result)
+
+    result = better_solution([1, 2, 3, 4, 5, 6], 8)
     print(result)
 
 main()
